89e29e00fa89f462e5dd3a0d3855b07d4ed95f80
antonpark <antonpark@wonikrobotics.com>
2016-06-30 07:10:21 +0000

    pi

--- launch/cam_client_launch.sh
+++ launch/cam_client_launch.sh
@@ -1,1 +1,1 @@
-gst-launch-1.0 -v tcpclientsrc host=1.1.20.136 port=5000 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false
+gst-launch-1.0 -v tcpclientsrc host=1.1.20.37 port=5000 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false
